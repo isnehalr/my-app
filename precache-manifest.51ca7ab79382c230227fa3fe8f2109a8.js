d10cd6c4dc30daab91c0c6421ba618c5963f9769
snehal-ramteke <snehal.ramteke@tieto.com>
2019-01-08 19:12:31 +0530

    Updates

---
+++ precache-manifest.51ca7ab79382c230227fa3fe8f2109a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/my-app/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "7844095f46c82289e3bb",
+    "url": "/my-app/static/js/main.7844095f.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/my-app/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "7844095f46c82289e3bb",
+    "url": "/my-app/static/css/main.2d69ebbe.chunk.css"
+  },
+  {
+    "revision": "311863af4fab4142fa79542c41cd2151",
+    "url": "/my-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.33ea465899b93a883e252d8f8142d840.js"
+  "/my-app/precache-manifest.51ca7ab79382c230227fa3fe8f2109a8.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/my-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
